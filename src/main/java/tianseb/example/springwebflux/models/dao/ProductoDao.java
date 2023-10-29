d5a7ff586287cc17fed6d63f6f5a5cdeabb7ad5b
AlassTian <sebastianalasia@kinandcarta.com>
2023-10-29 01:56:03 -0300

    Form Reactive Tymeleaf

--- src/main/java/tianseb/example/springwebflux/models/dao/ProductoDao.java
+++
@@ -1,7 +0,0 @@
-package tianseb.example.springwebflux.models.dao;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import tianseb.example.springwebflux.models.documents.Producto;
-
-public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
-}
